cb9111585460f6ff3b0aed582a3932e4cdd32ab5
NaganoMSL228 <9794@holbertonstudents.com>
2024-12-09 21:49:52 +0100

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_insert_left - Insère un nouveau nœud à gauche d'un parent
- *
- * @parent: Un pointeur vers le parent du nœud à insérer
+ * binary_tree_insert_left - Insère un nœud comme enfant gauche d'un autre nœud
+ * @parent: Pointeur vers le nœud parent pour insérer l'enfant gauche
  * @value: La valeur à stocker dans le nouveau nœud
  *
- * Return: Un pointeur vers le nouveau nœud inséré, ou NULL si
- * l'allocation échoue
- *
- * Cette fonction insère un nouveau nœud à gauche du nœud parent
- * spécifié.
+ * Return: Un pointeur vers le nœud créé, ou NULL en cas d'échec
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *new_node;
 
-{
-binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-if (new_node == NULL)
-{
+if (parent == NULL)
 return (NULL);
+
+new_node = binary_tree_node(parent, value);
+if (new_node == NULL)
+return (NULL);
+
+if (parent->left != NULL)
+{
+new_node->left = parent->left;
+parent->left->parent = new_node;
 }
 
-new_node->n = value;
-new_node->parent = parent;
-new_node->right = NULL;
 parent->left = new_node;
 
 return (new_node);
